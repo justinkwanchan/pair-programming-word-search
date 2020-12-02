e78b5c8c4186cca43217b9ea249cfd9ef0c65854
justinkwanchan <justinkwanchan@gmail.com>
2020-12-02 22:31:29 +0000

    Completed wordsearch.js and testing

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+function transpose(matrix) {
+  let newArray = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newArray.push([]);
+  }
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      newArray[i].push(matrix[j][i]);
     }
+  }
+
+  return newArray;
 }
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (word && l.includes(word)) {
+      return true;
+    }
+  }
+  
+  if (letters.length > 0) {
+    const verticalMatrix = transpose(letters);
+    const verticalJoin = verticalMatrix.map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+      if (word && l.includes(word)) {
+        return true;
+      }
+    }
+  }
+  
+  return false;
+};
+
+module.exports = wordSearch;
